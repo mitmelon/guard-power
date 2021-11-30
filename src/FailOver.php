12d4386c153671972fd7b1aa6412c9e776c5c000
Manomite <manomitehq@gmail.com>
2021-11-30 07:48:39 +0100

    Added failover
    Added failover

---
+++ src/FailOver.php
@@ -0,0 +1,14 @@
+<?php
+namespace GuardPower;
+
+/*
+
+    Title:        GuardPower Failover Class
+    Description:  Detect break in new release and auto fallback to the old release if the case requires.
+    Author:       Manomite LLC ( @manomite )
+
+*/
+class FailOver
+{
+   
+}
---
+++ src/Scanner.php
@@ -0,0 +1,14 @@
+<?php
+namespace GuardPower;
+
+/*
+
+    Title:        GuardPower Scanner Class
+    Description:  Scan PHP codes and ini files for vunerabilites and quickily notify the project developer for fixes before its discovered.
+    Author:       Manomite LLC ( @manomite )
+
+*/
+class Scanner
+{
+   
+}
